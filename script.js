4cee06919d42a9e480e43ada0e07af6f0a46d5d8
y1ann1s85 <yiannis.alexiou85@gmail.com>
2019-04-05 19:35:12 +0300

    cars running!

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
-const car = document.getElementById("car");
-car.style.right = "200px";
+const car = document.getElementById('car');
+car.style.right = '100%';
+
+// first step: display 1, 2, ... 100
+// Second step: display 1%, 2%,..., 100%
+// Last step: set the right position from 1%, ..., to 100%
+
+
+for(let i = 0; i <= 90; i++) {
+  setTimeout(() => {
+    car.style.right = i + '%';
+  }, 200*i)
+}
+
+const car2 = document.getElementById('car2');
+function moveCar(position) {
+  if (position > 100) return;
+  car2.style.right = position + '%';
+  setTimeout(() => moveCar(position + 1), 200);
+}
+moveCar(0);
