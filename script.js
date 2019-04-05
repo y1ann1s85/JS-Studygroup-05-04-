5a4f24c3f0ff2f5dab7d81323775c8253b64caa2
y1ann1s85 <yiannis.alexiou85@gmail.com>
2019-04-05 20:04:26 +0300

    Added start button

--- script.js
+++ script.js
@@ -1,21 +1,63 @@
-const car = document.getElementById('car');
-car.style.right = '100%';
+// const car = document.getElementById('car');
+// car.style.right = '100%';
 
 // first step: display 1, 2, ... 100
 // Second step: display 1%, 2%,..., 100%
 // Last step: set the right position from 1%, ..., to 100%
 
 
-for(let i = 0; i <= 90; i++) {
-  setTimeout(() => {
-    car.style.right = i + '%';
-  }, 200*i)
-}
+// for(let i = 0; i <= 90; i++) {
+//   setTimeout(() => {
+//     car.style.right = i + '%';
+//   }, 200*i)
+// }
 
-const car2 = document.getElementById('car2');
+// const car2 = document.getElementById('car2');
+// function moveCar(position) {
+//   if (position > 100) return;
+//   car2.style.right = position + '%';
+//   setTimeout(() => moveCar(position + 1), 200);
+// }
+// moveCar(0);
+
+// let myButton = document.getElementById("btn");
+
+// myButton.addEventListener("click", function () {
+//     // console.log("Clicked");
+//     for(let i = 0; i <= 90; i++) {
+//         setTimeout(() => {
+//           car.style.right = i + '%';
+//         }, 200*i)
+//       }
+      
+//       const car2 = document.getElementById('car2');
+//       function moveCar(position) {
+//         if (position > 100) return;
+//         car2.style.right = position + '%';
+//         setTimeout(() => moveCar(position + 1), 200);
+//       }
+//       moveCar(0);
+// });
+
+const car = document.getElementById('car');
+car.style.right = '1%';
+
+// first step: display 1, 2, ... 100
+// Second step: display 1%, 2%,..., 100%
+// Last step: set the right position from 1%, ..., to 100%
+
+const startButton = document.getElementById('start-button');
+
+let firstTime = true;
+startButton.addEventListener('click', () => {
+  if(firstTime) {
+    moveCar(0);
+    firstTime = false;
+  }
+});
+
 function moveCar(position) {
   if (position > 100) return;
-  car2.style.right = position + '%';
+  car.style.right = position + '%';
   setTimeout(() => moveCar(position + 1), 200);
 }
-moveCar(0);
